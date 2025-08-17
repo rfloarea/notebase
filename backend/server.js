897ebde6cb4032310ce06060df5abfd065f62d86
roe <rowen.floarea@proton.me>
2025-08-17 14:33:13 -0400

    this should have been a stash

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,22 @@ const PORT = process.env.PORT;
 
 connectDB();
 
+
+// routes
+server.get('/', findAllNotes);
+// server.post();
+// server.put();
+// server.delete();
+
 server.listen(PORT, () => {
 	console.log('Server listening on port 8080');
 });
 
-
+// mongoose schema and model
 const noteSchema = new mongoose.Schema({ title: String, content: String }, { timestamps: true });
 const Note = mongoose.model('Note', noteSchema);
 
-const newNote = new Note({ title: 'First note!', content: 'This is really cool!' });
-await newNote.save();
-
-const allNotes = await Note.find();
-console.log(allNotes);
-
+// db
 async function connectDB() {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
@@ -30,3 +32,35 @@ async function connectDB() {
 		console.log('Error connecting to database: ', error);
 	}
 };
+
+// express middleware
+async function findAllNotes(req, res) {
+	try {
+		const allNotes = await Note.find();
+		await res.status(200).send('GET request successful.');
+		console.log('GET request successful.');
+	} catch (error) {
+		await res.status(404).send('GET request error: 404');
+		console.log('GET request error: ', error);
+	}
+};
+
+//function createNewNote() {
+//
+//};
+//
+//function findOneNote('/:id') {
+//
+//};
+//
+//function updateOneNote(':/id') {
+//
+//};
+//
+//function deleteOneNote('/:id') {
+//
+//};
+//
+//function deleteAllNotes() {
+//
+//};
